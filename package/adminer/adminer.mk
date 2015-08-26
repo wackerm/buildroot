7ddec88221f2b290c28038c2d6eeb6d01196b2ed
wackerm <wacker@kreapptivo.de>
2015-08-26 20:41:22 +0200

    Minor fix for package adminer

--- package/adminer/adminer.mk
+++ package/adminer/adminer.mk
@@ -9,8 +9,12 @@ ADMINER_SITE=http://downloads.sourceforge.net/project/adminer/Adminer/Adminer%20
 ADMINER_SOURCE = adminer-$(ADMINER_VERSION)-mysql.php
 ADMINER_LICENSE = GPL-2
 
+define ADMINER_EXTRACT_CMDS
+	cp $(DL_DIR)/$(ADMINER_SOURCE) $(@D)
+endef
+
 define ADMINER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D $(@D)/$(ADMINER_SOURCE)
+	$(INSTALL) -m 0644 -D $(@D)/$(ADMINER_SOURCE) \
 		$(TARGET_DIR)/var/www/adminer/index.php
 endef
 
